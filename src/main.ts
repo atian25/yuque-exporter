76b7d743bd01ddefe8bb0082da4e131dac1e6d54
TZ | 天猪 <atian25@qq.com>
2022-10-28 14:59:43 +0000

    feat: init crawlee

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+// For more information, see https://crawlee.dev/
+import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
+import { router } from './routes.js';
+
+const startUrls = [ 'https://crawlee.dev' ];
+
+const crawler = new CheerioCrawler({
+  // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+  requestHandler: router,
+});
+
+await crawler.run(startUrls);
---
+++ src/routes.ts
@@ -0,0 +1,21 @@
+import { Dataset, createCheerioRouter } from 'crawlee';
+
+export const router = createCheerioRouter();
+
+router.addDefaultHandler(async ({ enqueueLinks, log }) => {
+  log.info('enqueueing new URLs');
+  await enqueueLinks({
+    globs: [ 'https://crawlee.dev/**' ],
+    label: 'detail',
+  });
+});
+
+router.addHandler('detail', async ({ request, $, log }) => {
+  const title = $('title').text();
+  log.info(`${title}`, { url: request.loadedUrl });
+
+  await Dataset.pushData({
+    url: request.loadedUrl,
+    title,
+  });
+});
